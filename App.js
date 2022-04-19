71dd79212638241688d9e98bfcc6e584eddcd900
arufiand <alfiandannyarmanta@gmail.com>
2022-04-19 21:00:21 +0700

    BMI Calculator Screen and Feature Fix

--- App.js
+++ App.js
@@ -3,11 +3,24 @@ import { LogBox } from 'react-native';
 import Router from './config/router';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 const App = () => {
   return (
-  <NavigationContainer>
-    <Router />
-  </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
--- pages/bmi_matrix/bmiMatrixScreen.js
+++ pages/bmi_matrix/bmiMatrixScreen.js
@@ -1,12 +1,116 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { View, TouchableOpacity, StyleSheet,SafeAreaView } from 'react-native'
+import { Text, Title,TextInput,Button,Modal, Portal,  } from 'react-native-paper';
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
 const BmiMatrixScreen = () => {
-  return (
-    <View>
-      <Text>BmiMatrixScreen</Text>
-    </View>
-  )
+
+    const [height, setHeight] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [bmi, setBmi] = useState(0);
+    const [result, setResult] = useState(0);
+    const [visible, setVisible] = React.useState(false);
+    const showModal = () => setVisible(true);
+    const hideModal = () => setVisible(false);
+    const containerStyle = {backgroundColor: 'white', padding: 30, height : 200, width : 300, borderRadius: 100/2, alignSelf : 'center', alignContent : 'center'};
+
+    const calculate = (height, weight) => {
+        var result = (parseFloat(weight) *1000) / (parseFloat(height)*parseFloat(height));
+        result = result.toFixed(2);
+        setBmi(result);
+        console.log(result);
+        setHeight(0);
+        setWeight(0);
+        showModal();
+        //showing result per BMI standards
+        if(result < 18.5){
+            setResult('Underweight');
+        }
+        else if (result >= 18.5 && result < 25) {
+            setResult('Normal Weight');
+        }
+        else if (result >= 25 && result < 30) {
+            setResult('Overweight');
+        }
+        else if (result >= 30) {
+            setResult('Obese');
+        }
+        else {
+            alert ('Incorrect Input!');
+            setResult('Wrong Input');
+        }
+    }
+
+    return (
+        <SafeAreaView>
+          <Title style= {styles.title}>BMI Calculator</Title>
+          <View style={{top : 50, alignItems : 'center' ,}}>
+            <TextInput
+                style ={{width : 250, margin : 20 }}
+                label="Height"
+                value={height}
+                onChangeText={height => setHeight(height)}
+                keyboardType='numeric'
+            />            
+            <TextInput
+                style ={{width : 250,margin : 20 }}
+                label="Weight"
+                value={weight}
+                onChangeText={weight => setWeight(weight)}
+                keyboardType='numeric'
+            />
+            <Button mode="contained" onPress={() => calculate(height, weight)}>
+                Press me
+            </Button>
+
+            <Portal>
+                <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                    <Title>BMI Results</Title>
+                    <Text>{bmi}</Text>
+                    <Text>{result}</Text>
+                </Modal>
+             </Portal>
+
+          </View>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+
+    submitButton: {
+       backgroundColor: '#ff6666',
+       padding: 10,
+       margin: 15,
+       height: 40,
+    },
+    submitButtonText:{
+       textAlign: "center",
+       color: 'white',
+      // fontWeight:"bold",
+       fontSize: 18,
+    },
+    output:{
+       textAlign: "center",
+       fontSize: 30,
+    },
+    title:{
+       paddingTop:30,
+       paddingBottom:10,
+       textAlign: "center",
+       fontSize: 30,
+       fontWeight:"bold",
+    },
+    resultText:{
+       paddingTop:20,
+       paddingBottom:10,
+       textAlign: "center",
+       fontSize: 30,
+       color: 'blue'
+    },
+    label:{
+       marginLeft: 15,
+    }
+ })
 
 export default BmiMatrixScreen
